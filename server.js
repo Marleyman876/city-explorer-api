8fa54a3d4020dbcac2ee1a37d4152e033c96699d
Marleyman876 <grant_garfield@yahoo.com>
2021-04-12 23:27:49 -0700

    pushing data to front end

--- server.js
+++ server.js
@@ -15,8 +15,21 @@ const PORT = process.env.PORT || 3001; // if 3002 is unavailable use 3001c
 
 app.get('/city_weather', (request, response) =>{
   //to get request and send response 
-  response.json(weatherData);
+  
+  response.json(mapOverdata());
 });
+
+function DailyForecast(day){
+  this.date = day.valid_date;
+  this.description = day.weather.description;
+
+}
+
+const mapOverdata = () => {
+ return weatherData.data.map(item =>{
+    return new DailyForecast(item)
+  })
+}
 
 // app.get('./city-explorer', (request, respone) =>{
 //   to get request and send response 
